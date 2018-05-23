de06411268d2e9fcc9b622bd8ef9d53c1e8c08c7
Steven Grimaldo <sgrimaldo@gsandf.com>
2018-05-22 22:22:54 -0500

    trying out some initial sum operations and type aliasing

--- index.elm
+++ index.elm
@@ -1,36 +1,22 @@
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (div, text)
+import Html.Attributes exposing (class)
+
+-- let's get started with a fake list of bills
+
+bills : List number
+bills =
+  [100, 300, 50, 50]
+
+-- we'll sum it up to to get the total
+
+total : number
+total =
+  List.sum bills
 
 main =
-  Html.beginnerProgram { model = model, view = view, update = update }
-
--- MODEL
-
-type alias Model = Int
-
-model : Model
-model =
-  0
-
--- UPDATE
-
-type Msg = Increment | Decrement
-
-update : Msg -> Model -> Model
-update msg model =
-  case msg of
-    Increment ->
-      model + 1
-
-    Decrement ->
-      model - 1
-
--- VIEW
-
-view : Model -> Html Msg
-view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (toString model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+  div [class "total"] [
+    text (
+      toString
+        total
+    )
+  ]
